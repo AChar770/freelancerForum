c7dd2935721b66426d370ddfa7de24ea6608266e
AChar770 <amandatcharles@yahoo.com>
2026-02-04 23:52:35 -0500

    first question completed

--- index.js
+++ index.js
@@ -5,8 +5,12 @@
  * @property {number} rate
  */
 
-// === Constants ===
+// === Constants === - Data Pool
 const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+function getRandomItem(array) {
+    return array[Math.floor(Math.random() * array.length];
+}
