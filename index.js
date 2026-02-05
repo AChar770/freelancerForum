aa3ecb6d966c7eafe63226c6c028e21acb1029cd
AChar770 <amandatcharles@yahoo.com>
2026-02-05 00:07:13 -0500

    create freelancer function added

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
 function getRandomItem(array) {
-    return array[Math.floor(Math.random() * array.length];
+    return array[Math.floor(Math.random() * array.length)];
 }
+
+  // Math.random() → number between 0 and 0.999...
+  // Multiply by array length → valid index range
+  // Math.floor → whole number index
+  
+  // This function creates ONE freelancer object
+// does NOT touch the DOM
+// It ONLY returns data
+function createFreelancer() {
+  return {
+    name: getRandomItem(NAMES),
+    occupation: getRandomItem(OCCUPATIONS),
+    rate: Math.floor(Math.random() * 100) + 20, // rate between 20–119
+  };
+}
+
+
